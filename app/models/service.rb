ddf4a6f5b599d5aa58330392feb94ce28941106a
cesarfuster <cfuster@uol.com.br>
2018-05-04 18:14:37 -0300

    Items dependence destroy Services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ApplicationRecord
   has_one :user
   has_many :user_tasks, through: :user
-  has_many :items
+  has_many :items, dependent: :destroy
   belongs_to :buyer, class_name: 'User'
   belongs_to :caregiver, class_name: 'User'
 
